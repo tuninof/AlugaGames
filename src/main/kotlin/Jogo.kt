4d1e069a0deabc62a1cfed12d35d7a30252b9796
tunino <tuninoff@gmail.com>
2023-06-22 21:27:03 -0300

    criando as data class

--- src/main/kotlin/Jogo.kt
+++ src/main/kotlin/Jogo.kt
@@ -1,2 +1,15 @@
-class Jogo {
+class Jogo(
+    val titulo: String,
+    val capa: String
+) {
+
+    val descricao = ""
+    override fun toString(): String {
+        return "meu Jogo: \n" +
+                "titulo: $titulo \n" +
+                "Capa: $capa \n" +
+                "Descricao: $descricao \n"
+    }
+
+
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,28 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse.BodyHandlers
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+
+fun main() {
+    val client: HttpClient = HttpClient.newHttpClient()
+    val request = HttpRequest.newBuilder()
+        .uri(URI.create("https://www.cheapshark.com/api/1.0/games?id=146"))
+        .build()
+    val response = client
+        .send(request, BodyHandlers.ofString())
+
+    val jason = response.body()
+    println(jason)
+
+
+
+    val meuJogo = Jogo("Batman: Arkham Asylum Game of the Year Edition",
+        "https:\\/\\/cdn.cloudflare.steamstatic.com\\/steam\\/apps\\/35140\\/capsule_sm_120.jpg?t=1681938587")
+
+    println(meuJogo)
+
+    val novoJogo = Jogo()
+
+
 }
